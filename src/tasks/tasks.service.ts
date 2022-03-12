29d534a4a968955636a568743ce1069e1c26e0ca
Yaroslav Prodanchuk <yaroslav.prodanchuk.external@ryd.one>
2022-03-12 11:41:52 +0200

    Create task method on service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { v4 as uuid } from 'uuid'
 
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 
 @Injectable()
 export class TasksService {
@@ -8,5 +9,18 @@ export class TasksService {
 
     getAllTasks(): Task[] {
         return this.tasks;
+    }
+
+    createTask(title: string, description: string): Task {
+        const task: Task = {
+            id: uuid(),
+            title,
+            description,
+            status: TaskStatus.OPEN
+        };
+
+        this.tasks.push(task);
+
+        return task;
     }
 }

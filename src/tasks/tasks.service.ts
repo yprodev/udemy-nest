44c59f3939d377c72fb23572ac6f398263be7788
Yaroslav Prodanchuk <yaroslav.prodanchuk.external@ryd.one>
2022-03-12 12:00:25 +0200

    Create and use task DTO

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { v4 as uuid } from 'uuid'
 
 import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -11,7 +12,9 @@ export class TasksService {
         return this.tasks;
     }
 
-    createTask(title: string, description: string): Task {
+    createTask(createTaskDto: CreateTaskDto): Task {
+        const { title, description } = createTaskDto;
+        
         const task: Task = {
             id: uuid(),
             title,

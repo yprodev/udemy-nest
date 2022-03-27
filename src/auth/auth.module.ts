a81f27103cd0c36179720ad584f2facf5e221061
Yaroslav Prodanchuk <yaroslav.prodanchuk.external@ryd.one>
2022-03-27 14:53:26 +0300

    Add JWT strategy and validate the token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,19 +5,28 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersRepository } from './users.repository';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { JWT_SECRET } from './auth.constants';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
-      secret: 'topSecret51',
+      secret: JWT_SECRET,
       signOptions: {
-        expiresIn: 3600
+        expiresIn: 3600 
       }
     }),
     TypeOrmModule.forFeature([UsersRepository]),
   ],
-  providers: [AuthService],
-  controllers: [AuthController]
+  providers: [
+    AuthService,
+    JwtStrategy
+  ],
+  controllers: [AuthController],
+  exports: [
+    JwtStrategy,
+    PassportModule
+  ]
 })
 export class AuthModule {}
